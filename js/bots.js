5d2f2885765792dde8500a6e13816f1d5d358f43
LAX18 <34464552+LAX18@users.noreply.github.com>
2018-01-27 16:54:55 -0500

    Added Command: Leave

--- js/bots.js
+++ js/bots.js
@@ -1,21 +1,21 @@
 //Demo/example code.
 
-var LAXBOT = new Bot("LAXBOT", "");
+var bot = new Bot("LAXBOT", "");
 
 //when UniChat is ready to load bots, register the bot(s). If you are doing multiple bots, you should put the registerBot commands in the same initializeBots block. Syntax: registerBot(bot_username, bot_header_char, callback). Note that callback is the name of the function *in quotes* to be called whenever a message starts with the command header.
 function initializeBots() {
-  LAXBOT.register();
+  bot.register();
 }
 
 //this is the function called whenever a message starts with the command header. We recommend function names of the format execute<bot_username> to prevent overlap.
-LAXBOT.executeCommand =  function(data) {
+bot.executeCommand =  function(data) {
   //This code initializes the variables:
   var poster = data.poster;
   var message = data.message;
   var timestamp = data.timestamp;
   var raw_timestamp = data.rawTimestamp;
   //if the first 4 characters of the message (starting from 0), minus the command header, are "ping", then highlight the user and say, "Pong!".
-  if (message.substring(0,4) == "ping") {
-  	this.respond(poster + ": Pong!");
+  if (message.substring(0,6) == "/leave") {
+  	this.respond(poster + " has left the room");
   }
 }
