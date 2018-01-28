9e9106830fe1eca36a1452238f8ab6a09ebe9006
_iPhoenix_ <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-01-28 11:32:19 -0500

    Update bots.js so that it fits the current spec.

--- js/bots.js
+++ js/bots.js
@@ -1,13 +1,12 @@
-//Demo/example code.
-
+//Create a bot, "testBot", that gets called whenever a message starts with "!"
 var testBot = new Bot("testBot", "!");
 
-//when UniChat is ready to load bots, register the bot(s). If you are doing multiple bots, you should put the registerBot commands in the same initializeBots block. Syntax: registerBot(bot_username, bot_header_char, callback). Note that callback is the name of the function *in quotes* to be called whenever a message starts with the command header.
+//When UniChat is ready to load bots, register the bot(s). If you are doing multiple bots, you should put the bot.register() commands in the same initializeBots block. Syntax: bot.register().
 function initializeBots() {
   testBot.register();
 }
 
-//this is the function called whenever a message starts with the command header. We recommend function names of the format execute<bot_username> to prevent overlap.
+//this is the function called whenever a message starts with the command header. 
 testBot.executeCommand =  function(data) {
   //This code initializes the variables:
   var poster = data.poster;
@@ -16,6 +15,6 @@ testBot.executeCommand =  function(data) {
   var raw_timestamp = data.rawTimestamp;
   //if the first 4 characters of the message (starting from 0), minus the command header, are "ping", then highlight the user and say, "Pong!".
   if (message.substring(0,4) == "ping") {
-  	this.respond(poster + ": Pong!");
+    testBot.respond(poster + ": Pong!");
   }
 }
