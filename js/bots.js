c0c519ab6e5dbcc7fc635ff2a44709386c4baba3
_iPhoenix_ <32944537+Legend-of-iPhoenix@users.noreply.github.com>
2018-01-31 16:52:28 -0500

    Should work?

--- js/bots.js
+++ js/bots.js
@@ -8,7 +8,7 @@ function initializeBots() {
 }
 
 //this is the function called whenever a message starts with the command header. We recommend function names of the format execute<bot_username> to prevent overlap.
-lax.executeCommand =  function(data) {
+lax.executeCommand = function(data) {
   //This code initializes the variables:
   var poster = data.poster;
   var message = data.message;
@@ -16,11 +16,11 @@ lax.executeCommand =  function(data) {
   var raw_timestamp = data.rawTimestamp;
  
   if (message.substring(0,7) == "laxinfo") {
-  	this.respond("Hello, Im Always Watching! ~ LAXBOT version: " + lax.version);
+  	lax.respond("Hello, Im Always Watching! ~ LAXBOT version: " + lax.version);
   }
   if (message.substring(0,9) == "pm LAXBOT DEACTIVATE" && poster = "LAX18") {
-    lax.deactivate()
-    this.respond("/pm LAX18 Bot Deactivated")
+    lax.deactivate();
+    lax.respond("/pm LAX18 Bot Deactivated");
   }
 }
 
